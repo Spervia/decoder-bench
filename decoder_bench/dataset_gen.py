a9810344ebddd5ef4ab7cf448a89c7c827467e4d
Spervia <1641324548@qq.com>
2025-12-17 18:27:51 +0800

    test_v2

--- decoder_bench/dataset_gen.py
+++ decoder_bench/dataset_gen.py
@@ -174,6 +174,7 @@ class DatasetGen(CompressedBinaryArrayStore):
                 pass
 
 
+
             if iterations == 0:
                 with h5py.File(f'{self.name}.h5', 'w') as file:
                     all_syndromes += len(syndromes)
